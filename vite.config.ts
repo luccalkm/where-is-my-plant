e3e44b0ed79ce1520703ef085aeea07fa4a29893
Lucca Motta <lucca.kleinsorgen@hotmail.com>
2025-06-28 12:27:46 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+        'icons/pwa-192x192.png',
+        'icons/pwa-512x512.png'
+      ],
+      manifest: {
+        name: 'Where Is My Plant',
+        short_name: 'My Plant',
+        description: 'Plante sua horta, onde quer que esteja!',
+        theme_color: '#4CAF50',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
