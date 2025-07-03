09342ea0925de885e9704bfa4d258941b40fab47
Lucca Motta <lucca.kleinsorgen@hotmail.com>
2025-07-03 01:35:48 -0300

    feat: update favicon and add PWA icons for better branding and user experience

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         'robots.txt',
         'apple-touch-icon.png',
         'icons/pwa-192x192.png',
-        'icons/pwa-512x512.png'
+        'icons/android-chrome-192x192.png'
       ],
       manifest: {
         name: 'Where Is My Plant',
@@ -25,13 +25,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icons/pwa-192x192.png',
+            src: 'icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'icons/pwa-512x512.png',
+            src: 'icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
